da196684fa9d823260e32c8c6c8cba6771276dd5
+KOT <leonardongc@gmail.com>
2021-05-13 11:28:52 -0300

    added function, conversion bugs

--- coinChecker.js
+++ coinChecker.js
@@ -7,24 +7,47 @@ const matcherFee = 0.003
 
 //função para coleta de dados nas apis
 async function getData(address){
-    let response = await fetch(address);
+    let response = await fetch(address)
 
     if (response.ok) {
-        let json = response.json();
+        let json = response.json()
         return json
     }else {
-        console.log("Error at getData func - DEURUIM"); //ainda tentando descobrir em que parte causa erro quando a coleta de dados é grande
+        console.log("Error at getData func - DEURUIM") //ainda tentando descobrir em que parte causa erro quando a coleta de dados é grande
 }
 
+}
+//função que verifica orderbook
+function checkBids(bids,decimals,result,index=0){
+    if(!bids[index]){
+        result.profit -= matcherFee
+        return(result)
+    }
+    let bidRatio = result.finalBalance/(bids[index].amount/10**decimals)
+    if(bidRatio<1.0){
+        
+        result.endPrice = bids[index].price/(10**8)
+        result.profit += (result.endPrice*bidRatio - matcherFee)
+        result.finalBalance = 0
+        return(result)
+
+        
+    }else{
+        result.endPrice = bids[index].price/(10**8)
+        result.profit += result.endPrice
+        result.finalBalance -= bids[index].amount/10**decimals
+        return(checkBids(bids,decimals,result,index ++))
+    }
+    
 }
 
 //endereços para testes
-const testAddress = '3PH2vS8qzdGf66hDskiRMk3vrUuZBUzpSau'
-const nodeBR ='3P1xU8QSBk2gDQkepGYu24tTfwj4FgS7avv'
-const wavesnode = '3P23fi1qfVw6RVDn4CH2a5nNouEtWNQ4THs'
+const testAddress = '3PH2vS8qzdGf66hDskiRMk3vrUuZBUzpSau'    //meu endereço
+const nodeBR ='3P1xU8QSBk2gDQkepGYu24tTfwj4FgS7avv'          //Node brasileiro do time Waves - faz um lease lá!
+const wavesnode = '3P23fi1qfVw6RVDn4CH2a5nNouEtWNQ4THs'      //node gringo com uma quntidade massiva de moedas que ainda não consiguimos tratar
 
 //função checagem das moedas - aparentemente não consegue tratar uma carteira com muitas moedas
-let checkCoins = async function(wallet){
+async function checkCoins(wallet){
         let dumpfolio = []
         let getCoins = await getData(coinsAPI + wallet)
         getCoins = getCoins.balances
@@ -36,16 +59,21 @@ let checkCoins = async function(wallet){
             let currentBalance = getCoins[i].balance/(10**decimals)
             let sponsored = (getCoins[i].minSponsoredAssetFee  ? "SPONSORED"  : "UNSPONSORED")
             let orderbook = await getData(`https://matcher.waves.exchange/matcher/orderbook/${coinId}/WAVES`)
-            let price = (orderbook.bids[0] ? orderbook.bids[0].price*(10**(decimals -16)) : 0)// esse método ainda pode ser enganoso caso "orderbook.bids[0].amount < currentBalance" correção a ser implementada
-            let minWorthSell = matcherFee/price
+            let afterSell = checkBids(orderbook.bids,decimals, {"finalBalance": currentBalance, "endPrice":0, "profit": 0})
+            console.log(`Name: ${coinName} | sponsored? ${sponsored} | ${JSON.stringify(afterSell)}`)
+            //let price = (orderbook.bids[0] ? orderbook.bids[0].price*(10**(decimals -16)) : 0)// esse método ainda pode ser enganoso caso "orderbook.bids[0].amount < currentBalance" correção a ser implementada
+            //let minWorthSell = matcherFee/price
+            
+            //bloco de logs
+            
             //let line = `Name: ${coinName} | sponsored? ${sponsored} | minimum value worth selling: ${minWorthSell} | Your Balance: ${currentBalance} ${(currentBalance > minWorthSell ? "SELL" : "HODL")}\n`
             //console.log(line)
-            if(currentBalance > minWorthSell){console.log(`${coinName} is SELLABLE for ${currentBalance*price -matcherFee}`)}
+            //if(currentBalance > minWorthSell){console.log(`${coinName} is SELLABLE for ${currentBalance*price -matcherFee}`)}
             //if(sponsored == "SPONSORED"){console.log(`${coinName} is SPONSORED`)}
-            //dubugging ainda
+            //debugging ainda
         }
 
         return(dumpfolio)
     } 
 
-checkCoins(wavesnode)
+checkCoins(testAddress)
